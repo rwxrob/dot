1c96a3afcbecb3dd66248fa00d666b4a3b203880
rwxrob <rwx@robs.io>
2020-11-24 06:41:01 -0500

    gotests added.

---
+++ common/shell/colors-basic.bash
@@ -0,0 +1,12 @@
+
+# foregrounds
+
+export black=$'\033[30m'
+export red=$'\033[31m'
+export green=$'\033[32m'
+export yellow=$'\033[33m'
+export blue=$'\033[34m'
+export magenta=$'\033[35m'
+export cyan=$'\033[36m'
+export white=$'\033[37m'
+
--- common/shell/termcap-colors.bash
+++ common/shell/termcap-colors.bash
@@ -7,4 +7,4 @@ export LESS_TERMCAP_me=$reset
 export LESS_TERMCAP_se=$reset
 export LESS_TERMCAP_so=$blue
 export LESS_TERMCAP_ue=$reset
-export LESS_TERMCAP_us=$violet
+export LESS_TERMCAP_us=$'\033[4m'
