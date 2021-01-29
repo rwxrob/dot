8b470a3b1a12587a2ffb49f1fcda8188cc361da4
rwxrob <rob@rwx.gg>
2021-01-29 07:19:59 -0500

    Added wipe to clear both chat buffer and screen.

--- live/bashrc/alias-func.bash
+++ live/bashrc/alias-func.bash
@@ -1,9 +1,13 @@
 
-alias kali='ssh kali'
 alias pico='ssh pico'
 alias nicks="s /names"
 
-alias c="clear"
+alias c="/usr/bin/clear"
 alias cl="s /buffer clear"
 alias x="exit"
 alias sl="sl -e"
+
+wipe() {
+  printf  $'\033[2J\033[;H'
+  wee '/buffer clear'
+} && export -f wipe
