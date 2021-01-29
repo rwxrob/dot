a11fb7a80fc1ebbb944c6dfd6ef4e5ca0757135e
rwxrob <rob@rwx.gg>
2021-01-29 07:40:25 -0500

    Fixed \034 bug in clear alias.

--- live/bashrc/alias-func.bash
+++ live/bashrc/alias-func.bash
@@ -2,12 +2,12 @@
 alias pico='ssh pico'
 alias nicks="s /names"
 
-alias c="/usr/bin/clear"
+alias c="printf  $'\033[2J\033[;H'"
 alias cl="s /buffer clear"
 alias x="exit"
 alias sl="sl -e"
 
 wipe() {
-  printf  $'\033[2J\033[;H'
+  printf  "\033[2J\033[;H"
   wee '/buffer clear'
 } && export -f wipe
