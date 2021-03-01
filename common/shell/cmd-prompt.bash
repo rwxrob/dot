a2725df09f2803ba4a820d71c72a107dd75869ba
rwxrob <rob@rwx.gg>
2021-03-01 11:00:13 -0500

    Add branch to command prompt

--- common/shell/cmd-prompt.bash
+++ common/shell/cmd-prompt.bash
@@ -12,6 +12,8 @@ export glyphflames=$'\ue0c0'
 export glyphrounded=$'\ue0b4'
 export glyphbits=$'\ue0c6'
 export glyphhex=$'\ue0cc'
+export glyphbranch=$'\ue0a0'
+export glyphrarrow=$'\u279c'
 
 export PS1w=$'\033[38;5;33m'
 export PS1u=$'\033[38;2;0;165;0m'
@@ -23,6 +25,7 @@ export PS1P=$'\033[38;2;255;0;0m'
 export PS1U=$PS1P
 
 export gruv_orange=$'\033[38;2;214;93;13m'
+export gruv_purple=$'\033[38;2;211;134;155m'
 
 if [[ $EUID == 0 ]]; then
   export PS1='\[$PS1U\]\u\[$PS1a\]@\[$PS1h\]\h\[$PS1c\]:\[$PS1w\]\w\[$PS1P\]#\[\033[00m\] '
@@ -31,7 +34,7 @@ else
 fi
 
 ps1min () {
-  export PS1='\[\033[0;34m\]\W\[\033[0;33m\]$\[\033[00m\] '
+  export PS1='\[\033[0;34m\]\W$(__git_ps1 "${PS1c}:${gruv_purple}%s")\[\033[0;33m\]\$\[\033[00m\] '
   #export PS1='$ '\033[0;35m
 }
 
