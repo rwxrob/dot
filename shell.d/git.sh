27c0bb6cb9f39e974abbf5b6109e52406117942d
rwxrob <rob@rwx.gg>
2021-03-23 14:54:26 -0400

    Add $HOME/repos as REPOS default

--- shell.d/git.sh
+++ shell.d/git.sh
@@ -1,2 +1,5 @@
+# Both REPOS and GITUSER can (and often should) be overriden in
+# personal, private, or work imported rc modules later.
 
+export REPOS="$HOME/repos"
 havecmd git && export GITUSER="$(git config --global user.name)"
