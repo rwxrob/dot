f6111a313bc3a3c8def340c4175f501ff3134257
rwxrob <rob@rwx.gg>
2021-03-26 14:36:40 -0400

    Fix #5 tab completion

--- shell.d/settings.bash
+++ shell.d/settings.bash
@@ -2,7 +2,7 @@
 #set -o noclobber            # paranoid? use >| for everything
 shopt -s checkwinsize
 shopt -s expand_aliases
-shopt -s nullglob
+#shopt -s nullglob           # kills completion (for some)
 shopt -s globstar
 shopt -s dotglob
 shopt -s extglob
