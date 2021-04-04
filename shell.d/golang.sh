a9e06764aaa6f9d9e79ec076e26c02d70ffad1fd
rwxrob <rob@rwx.gg>
2021-04-04 02:56:41 -0400

    Reorder the lines of shell.d/golang.sh

--- shell.d/golang.sh
+++ shell.d/golang.sh
@@ -1,7 +1,7 @@
 export GOPRIVATE="github.com/$GITUSER/*,gitlab.com/$GITUSER/*"
-export GOBIN="$HOME/.local/go/bin"
-mkdir -p $GOBIN
 export GOPATH="$HOME/.local/go"
+export GOBIN="$HOME/.local/go/bin"
 export GOPROXY="direct"
 export CGO_ENABLED=0
+mkdir -p $GOBIN
 
