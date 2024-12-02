d0c99fab6bd3d6bad9c5306d8902b2b1ce8b1c09
Rob Muhlestein <rob@rwx.gg>
2024-12-02 06:12:03 -0500

    save

--- vim/init.lua
+++ vim/init.lua
@@ -2,4 +2,8 @@ vim.opt.runtimepath:prepend(vim.env.HOME .. '/.vim')
 vim.opt.runtimepath:append(vim.env.HOME .. '/.vim/after')
 vim.opt.packpath = vim.opt.runtimepath:get()
 vim.cmd('source ~/.vimrc')
+vim.cmd [[
+  highlight Normal guibg=NONE
+  highlight NonText guibg=NONE
+]]
 
