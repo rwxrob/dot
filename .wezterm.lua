8dcacd1c7d3504869aeda686d56f4995e51fe816
Rob Muhlestein <rob@rwx.gg>
2024-12-22 19:56:23 -0500

    save

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,52 +1,45 @@
 local wezterm = require("wezterm")
-
 return {
-
-  font_size = 34,
-  font = wezterm.font("UbuntuMono Nerd Font"),
+  window_close_confirmation = 'NeverPrompt',
+  default_prog = {'/opt/homebrew/bin/bash','--login'},
+  color_scheme = 'Gruvbox Material (Gogh)',
+  font = wezterm.font('UbuntuMono Nerd Font'),
+  font_size = 35,
+  colors = {
+    cursor_bg = '#928374',
+    cursor_border = '#928374',
+  },
 
   window_padding = {
-   left = 0,
+   left = 40,
    right = 0,
    top = 0,
-   bottom = 0,
+   bottom = 2,
   },
 
   term = "xterm-256color",
-  animation_fps = 60,
-  max_fps = 60,
-  window_decorations = "RESIZE",
 
-  enable_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+  ----------- rwxrob streaming stuff, fyi --------------
 
-  colors = {
-     foreground = "#d4be98", -- Default text color
-     cursor_bg  = "#d4be98", -- Default text color
-     cursor_border = "#d4be98", -- Default text color
-     background = "#282828", -- Background color
-     selection_bg = "#45403d" ,
+  --font_size = 41,
+  --initial_cols = 74,
+  --initial_rows = 22,
 
-    ansi = {
-      "#282828", -- Black (Normal)
-      "#cc241d", -- Red (Normal)
-      "#98971a", -- Green (Normal)
-      "#d79921", -- Yellow (Normal)
-      "#458588", -- Blue (Normal)
-      "#b16286", -- Magenta (Normal)
-      "#689d6a", -- Cyan (Normal)
-      "#a89984", -- White (Normal)
-    },
-    brights = {
-      "#928374", -- Bright Black
-      "#fb4934", -- Bright Red
-      "#b8bb26", -- Bright Green
-      "#fabd2f", -- Bright Yellow
-      "#83a598", -- Bright Blue
-      "#d3869b", -- Bright Magenta
-      "#8ec07c", -- Bright Cyan
-      "#ebdbb2", -- Bright White
-    }
-  }
+  --[[
+  -- exactly position
+  wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():set_position(0, 130)
+end),
+  ]]--
+
+  --window_decorations = "RESIZE",
+  --enable_tab_bar = false,
+  --hide_tab_bar_if_only_one_tab = true,
 
 }
+
+
+
+
+
