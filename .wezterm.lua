94c9308e90bad6e0842070da57405be5fd534814
Rob Muhlestein <rob@rwx.gg>
2024-12-22 20:18:55 -0500

    save

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,34 +9,9 @@ return {
     cursor_bg = '#928374',
     cursor_border = '#928374',
   },
-
-  window_padding = {
-   left = 40,
-   right = 0,
-   top = 0,
-   bottom = 2,
-  },
-
   term = "xterm-256color",
-
-  ----------- rwxrob streaming stuff, fyi --------------
-
-  --font_size = 41,
-  --initial_cols = 74,
-  --initial_rows = 22,
-
-  --[[
-  -- exactly position
-  wezterm.on('gui-startup', function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():set_position(0, 130)
-end),
-  ]]--
-
-  --window_decorations = "RESIZE",
-  --enable_tab_bar = false,
-  --hide_tab_bar_if_only_one_tab = true,
-
+  window_decorations = "RESIZE",
+  hide_tab_bar_if_only_one_tab = true,
 }
 
 
