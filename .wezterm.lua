3a0ea2445d3235722efb1ef6b79637b4fe1649a6
Rob Muhlestein <rob@rwx.gg>
2025-01-20 21:43:12 -0500

    save

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,19 +5,19 @@ return {
   default_prog = {'/opt/homebrew/bin/bash','--login'},
   color_scheme = 'Gruvbox Material (Gogh)',
   font = wezterm.font('UbuntuMono Nerd Font'),
-  font_size = 28,
+  font_size = 35.2,
   colors = {
     cursor_bg = '#928374',
     cursor_border = '#928374',
   },
   term = "xterm-256color",
-  window_decorations = "RESIZE",
+--  window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
   window_padding = {
    left = 10,
    right = 0,
-   top = 20,
-   bottom = 0,
+   top = 0,
+   bottom = 10,
   },
 }
 
